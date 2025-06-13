1bb2983cd8b17d2cd6e660500ef80b40660597e7
Karan <jakharkaran94@gmail.com>
2025-06-13 18:55:58 +0000

    function calculating grad norm and grad max

--- tools/util.py
+++ tools/util.py
@@ -1,5 +1,24 @@
 import torch
 
+def grad_norm(model):
+    total_norm = 0
+    parameters = [p for p in model.parameters() if p.grad is not None and p.requires_grad]
+    for p in parameters:
+        param_norm = p.grad.detach().data.norm(2)
+        total_norm += param_norm.item() ** 2
+    total_norm = total_norm ** 0.5
+    return total_norm
+
+
+def grad_max(model):
+    max_grad = 0
+    parameters = [p for p in model.parameters() if p.grad is not None and p.requires_grad]
+    for p in parameters:
+        param_max = torch.max(torch.abs(p.grad.detach().data))
+        if max_grad < param_max.item():
+            max_grad = param_max.item()
+    return param_max
+
 class SpectralDifferentiator:
     def __init__(self, nx, ny, Lx=2*torch.pi, Ly=2*torch.pi, device='cpu', dtype=torch.float32):
         """
